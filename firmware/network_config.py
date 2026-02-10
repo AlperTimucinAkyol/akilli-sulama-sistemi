0274f20b8a273ec58feb96d7b52204b8c7da86d3
Erdinç Topuz <115416214+Erdinctpz@users.noreply.github.com>
2026-02-10 14:21:44 +0300

    WiFi bağlantısı ayarlamaları yapıldı

---
+++ firmware/network_config.py
@@ -0,0 +1,27 @@
+import network
+import time
+
+# --- Ayarlar ---
+SSID = "internet_adi"
+PASSWORD = "internet_sifresi"
+
+def wifi_baglan():
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(False) 
+    time.sleep(1)
+    wlan.active(True)
+    wlan.connect(SSID, PASSWORD)
+    
+    print("WiFi'a bağlanılıyor...", end="")
+    timeout = 0
+    while not wlan.isconnected() and timeout < 10:
+        print(".", end="")
+        time.sleep(1)
+        timeout += 1
+        
+    if wlan.isconnected():
+        print("\nBağlandı! IP:", wlan.ifconfig()[0])
+        return True
+    else:
+        print("\nBağlanamadı!")
+        return False
