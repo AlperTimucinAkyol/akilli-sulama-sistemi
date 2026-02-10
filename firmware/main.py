42d803a764b4a154fa843c2adb22349efb5be291
Erdinç Topuz <115416214+Erdinctpz@users.noreply.github.com>
2026-02-10 14:19:51 +0300

    Sulama kontrolü için MQTT servisi uygulandı.
    MQTT işlevselliği eklendi

--- firmware/main.py
+++ firmware/main.py
@@ -1,28 +1,42 @@
-from machine import ADC, Pin
 import time
+import json
+from umqtt.simple import MQTTClient
+import network_config  # wifi_baglan burada
+import config          # Tüm ayarlar burada
+from sensors import SensorManager
 
-from config import NEM_ESIK_ADC, OKUMA_ARALIGI_S
+sm = SensorManager(config.DHT_PIN)
 
-adc = ADC(Pin(34))           
-pump_relay = Pin(25, Pin.OUT) 
-adc.atten(ADC.ATTN_11DB)     
-
-def oku_nem():
-    return adc.read()
-
-def sulama_kontrol(nem_degeri):
-    if nem_degeri > NEM_ESIK_ADC:
-        pump_relay.on()
-        return True
-    else:
-        pump_relay.off()
-        return False
+def sub_cb(topic, msg):
+    try:
+        print(f"Yeni Komut Geldi [{topic}]: {msg}")
+        data = json.loads(msg)
+        
+        if data.get('watering') == True:
+            print(">>> SULAMA BAŞLADI!")
+        else:
+            print(">>> SULAMA DURDURULDU!")
+    except Exception as e:
+        print("Komut işleme hatası:", e)
 
 
-print("Akıllı Sulama Sistemi Başlatıldı")
-while True:
-    nem = oku_nem()
-    sulama = sulama_kontrol(nem)
-    durum = "ASIYOR" if sulama else "YETERLI"
-    print(f"[{time.ticks_ms() // 1000}] Nem: {nem} → {durum}")
-    time.sleep(OKUMA_ARALIGI_S)
+if network_config.wifi_baglan():
+    client = MQTTClient(config.CLIENT_ID, config.MQTT_SERVER)
+    client.set_callback(sub_cb)
+    client.connect()
+    client.subscribe(config.TOPIC_SUB)
+    
+    last_publish = 0
+    
+    while True:
+        client.check_msg()
+        
+        if (time.time() - last_publish) >= 10:
+            data = sm.get_data()
+            if data:
+                data["device_id"] = config.CLIENT_ID
+                client.publish(config.TOPIC_PUB, json.dumps(data))
+                print("Veri gönderildi:", data)
+            last_publish = time.time()
+        
+        time.sleep(0.1)
