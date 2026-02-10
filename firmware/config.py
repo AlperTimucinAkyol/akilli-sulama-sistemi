e7cf5cf161f2efd39280bff2616a00e01bd7edc7
Erdinç Topuz <115416214+Erdinctpz@users.noreply.github.com>
2026-02-10 14:23:03 +0300

    WiFi ve MQTT yapılandırma ayarlarını eklendi.

--- firmware/config.py
+++ firmware/config.py
@@ -1,2 +1,12 @@
-NEM_ESIK_ADC = 2000  
-OKUMA_ARALIGI_S = 10  
+# --- WiFi Ayarları ---
+SSID = "internet_adi"
+PASSWORD = "internet_sifresi"
+
+# --- MQTT Ayarları ---
+MQTT_SERVER = "MQTT cihazinin IP adresi"
+CLIENT_ID = "esp32_tarla_1"
+TOPIC_SUB = b"esp32/komut"
+TOPIC_PUB = b"esp32/sensor"
+
+# --- Donanım Ayarları ---
+DHT_PIN = 4
